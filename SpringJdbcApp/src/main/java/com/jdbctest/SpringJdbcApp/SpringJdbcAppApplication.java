fdec4a773335c864a8facd9de5dd55ff96f15829
mk079711 <mohak.kumar@cerner.com>
2021-08-13 11:26:42 +0530

    First Commit

--- SpringJdbcApp/src/main/java/com/jdbctest/SpringJdbcApp/SpringJdbcAppApplication.java
+++ SpringJdbcApp/src/main/java/com/jdbctest/SpringJdbcApp/SpringJdbcAppApplication.java
@@ -1,13 +1,13 @@
-package com.app.techworm.main;
+package com.jdbctest.SpringJdbcApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SpringJdbcAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SpringJdbcAppApplication.class, args);
 	}
 
 }
